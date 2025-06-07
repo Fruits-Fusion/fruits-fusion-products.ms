03a83b464b4a98343107d1f34c99592d203491fc
Josué Rojas <sjosuerojasc@gmail.com>
2025-06-07 18:40:25 -0500

    Feat: Integration prisma services

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from '../common/pagination/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -14,18 +15,18 @@ export class ProductsController {
   }
 
   @MessagePattern('findAllProducts')
-  findAll() {
-    return this.productsService.findAll();
+  findAll(paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
   }
 
   @MessagePattern('findOneProduct')
-  findOne(@Payload() id: number) {
+  findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
   @MessagePattern('updateProduct')
   update(@Payload() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(updateProductDto.id, updateProductDto);
+    return this.productsService.update(updateProductDto);
   }
 
   @MessagePattern('removeProduct')
